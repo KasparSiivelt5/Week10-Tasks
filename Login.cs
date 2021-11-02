cae0c20591d8b2c69f7d80fe5773c93d8e2b5b88
KasparSiivelt5 <89830554+KasparSiivelt5@users.noreply.github.com>
2021-11-02 15:48:32 +0200

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Login");
+            string UserLogin = Console.ReadLine();
+            Console.WriteLine("Password");
+            string UserPassword = Console.ReadLine();
+
+            if (UserLogin != "admin" || UserPassword != "admin1234")
+            {
+                Console.WriteLine("The login is incorrect");
+            }
+            else
+            {
+                Console.WriteLine("Success");
+            }    
+        
+        }
+
+    }
+}
